71b1f6641c9a80aba3aa4a0882eca16c9907d5d9
Alex Thacker <thacker.alex@yahoo.com>
2019-04-29 13:10:23 -0500

    navbar setup

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 export default class NavBar extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    render() {
+        return (
+            <nav className="navbar navbar-light light-blue flex-md-nowrap p-0 shadow">
+                <ul className="nav nav-pills nav-fill">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/home">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/postpool">Post Pool</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/findpool">Find Pool</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/mysaveditems">My Saved Items</Link>
+                    </li>
+                </ul>
+                {/* <Link className="mr-1" onClick={() => sessionStorage.clear()} to="/">Logout</Link> */}
+            </nav>
+        )
+    }
 }
