de339dd1c69acdadab20def7603aef4ee56b7f26
Alex Thacker <thacker.alex@yahoo.com>
2019-04-29 12:55:56 -0500

    setup for files

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ApplicationViews extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
