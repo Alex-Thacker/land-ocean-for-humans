e40fe1cea4ec15bcd5a5b469258d22f71724ed56
Alex Thacker <thacker.alex@yahoo.com>
2019-05-21 15:48:51 -0500

    spelling correction

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -15,7 +15,7 @@ export default class Home extends Component {
         <div className="homeCss">
           <h1>Land Ocean for Humans</h1>
           <h3>Created by Alex Thacker</h3>
-          <p className={this.state.hideClass}>This application has two main features. The first is designed for people with swimming pools. The app will let them post their pool and allow the public to use it, basically turing their private pool into a public one. They have the option to charge an entry fee, set times of availability, and any other detail they would like. The second feature is for people without a pool. It allows them to find pools that other people have posted. Once they find a pool they like, they're able to save it to their personal profile. Enjoy!</p>
+          <p className={this.state.hideClass}>This application has two main features. The first is designed for people with swimming pools. The app will let them post their pool and allow the public to use it, basically turning their private pool into a public one. They have the option to charge an entry fee, set times of availability, and any other detail they would like. The second feature is for people without a pool. It allows them to find pools that other people have posted. Once they find a pool they like, they're able to save it to their personal profile. Enjoy!</p>
           <button className={`btn btn-primary ${this.state.showClass}`} onClick={() => this.setState({
             hideClass: "",
             showClass: "hideAbout"
